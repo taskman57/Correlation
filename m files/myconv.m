e4e04011408344fe173c7477532b997ca9f5c69b
Hossein Moradi <hossein.moradi.sarvandi@gmail.com>
2026-02-15 20:29:14 +0100

    perf(myconv): replace shift-based convolution with index-based MAC implementation

--- m files/myconv.m
+++ m files/myconv.m
@@ -8,19 +8,17 @@ function c = myconv(a,b)
   a_was_row = isrow(a);
   a = a(:).';                                           % force row
   b = b(:).';                                           % force row
-  conv_len = length(b) + length(a)-1;
-  zero_pad_a = length(a) - 1;
-  zero_pad_b = length(b) - 1;
-  printf(' flipped and zero padded.\n\r');
-  a = [zeros(1,zero_pad_b) a];
-  b = [fliplr(b) zeros(1,zero_pad_a)];
+  lena = length(a);
+  lenb = length(b);
+  conv_len = lenb + lena-1;
   c = zeros(1, conv_len);
   if conv_len -1 > 0                                    % a & b are vectors
       for i=1:conv_len                                  % number of shifts
-        for j=1:length(b)                               % MAC part
-          c(i) = c(i) + a(j)*b(j);
+        min_lim = max(1,(i+1-lenb));
+        max_lim = min(i, lena);
+        for j=min_lim:max_lim                           % MAC part
+          c(i) += a(j)*b(i-j+1);
         endfor
-        b = [0 b(1:end-1)];                             % shift b to right
       endfor
   else
     c= b*a;
