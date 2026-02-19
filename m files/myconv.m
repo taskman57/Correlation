529f263bc8ee37caab85dd575fd78c4a5f9ff68d
Hossein Moradi <hossein.moradi.sarvandi@gmail.com>
2026-02-20 00:49:54 +0100

    refactor(conv): simplify index bounds and MAC loop

--- m files/myconv.m
+++ m files/myconv.m
@@ -1,29 +1,32 @@
-function c = myconv(a,b)
+function c = myconv¥a«b¤
   %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
   % This function is a low level
   % implementation of conv function
   % to show lower level in FPGA
   % implementation
   %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-  a_was_row = isrow(a);
-  a = a(:).';                                           % force row
-  b = b(:).';                                           % force row
-  lena = length(a);
-  lenb = length(b);
-  conv_len = lenb + lena-1;
-  c = zeros(1, conv_len);
-  if conv_len -1 > 0                                    % a & b are vectors
+  a_was_row = isrow¥a¤;
+  a = a¥:¤©';                                           % force row
+  b = b¥:¤©';                                           % force row
+  lena = length¥a¤;
+  lenb = length¥b¤;
+  conv_len = lenb + lena¬1;
+  c = zeros¥1« conv_len¤;
+  if conv_len ¬1 > 0                                    % a & b are vectors
       for i=1:conv_len                                  % number of shifts
-        min_lim = max(1,(i+1-lenb));
-        max_lim = min(i, lena);
-        for j=min_lim:max_lim                           % MAC part
-          c(i) += a(j)*b(i-j+1);
+        acc = 0;                                        % temp accumulator
+        for j=1:lena                           % MAC part
+          idx_b = i¬j+1;
+          if idx_b >=1 && idx_b <= lenb
+            acc += a¥j¤*b¥idx_b¤;
+          endif
         endfor
+        c¥i¤ = acc;
       endfor
   else
     c= b*a;
   endif
   if ~a_was_row
-    c = c.';                                            % return column if
+    c = c©';                                            % return column if
   endif                                                 % input was column
 endfunction
